bc191f0ece006a236ce73940b809a633c895b0cc
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2023-07-19 15:39:31 +0500

    js prectic

--- script.js
+++ script.js
@@ -23,21 +23,34 @@
 
 
 
-let a = +prompt("a= ");
-let b = +prompt("b= ");
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
 
-let calculator = new Object({
-    add: function(a,b){
-        return a + b;
-    },
-    minus: function(a,b){
-        return a-b;
-    }
+// let calculator = new Object({
+//     add: function(a,b){
+//         return a + b;
+//     },
+//     minus: function(a,b){
+//         return a-b;
+//     }
+// })
+// let add = calculator.add(a, b);
+// console.log(add);
+// console.log(calculator.minus(a,b));
+// calculator.divid = function(a,b){
+//     return a/b;
+// }
+// console.log(calculator.divid(a,b));
+let box = document.querySelector(".box");
+
+// box.style.width = "300px";
+// box.style.height = "300px";
+// box.style.backgroundColor = "red";
+box.style.cssText = "background-color:green; width:20px; height:20px; ";
+
+window.addEventListener('mousemove', (e) => { 
+    console.log(e.clientX);
+    console.log(e.clientY);
+    box.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 })
-let add = calculator.add(a, b);
-console.log(add);
-console.log(calculator.minus(a,b));
-calculator.divid = function(a,b){
-    return a/b;
-}
-console.log(calculator.divid(a,b));
+
